b7265e1ce66b5fad878af33d8306d6ae17705dbc
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-10-29 13:20:15 -0300

    Adicionar vídeo

--- OBM_2015_fase1/main.py
+++ OBM_2015_fase1/main.py
@@ -1,28 +1,32 @@
+import math
 from manim import *
+
 
 class Problema(Scene):
     def construct(self) -> None:
         # Título
-        titulo = Tex(f'OBM (2015)')
+        titulo = Tex(f"OBM (2015)")
         titulo.scale(2)
 
-        #Enunciado
-        Enunciado = Tex(f'Qua é o valor da expressão:\\\\$2015^2 - 2015 \cdot 2014 - 2014^2 + 2014 \cdot 2015$')
+        # Enunciado
+        Enunciado = Tex(
+            f"Qua é o valor da expressão:\\\\$2015^2 - 2015 \cdot 2014 - 2014^2 + 2014 \cdot 2015$"
+        )
         Enunciado.scale(1.5)
 
-        Parte1 = Tex(f'Qua é o valor da expressão:\\\\$a^2 - ab - b^2 + ab$')
+        Parte1 = Tex(f"Qua é o valor da expressão:\\\\$a^2 - ab - b^2 + ab$")
         Parte1.scale(2)
 
-        Parte2 = Tex(f'Qua é o valor da expressão:\\\\$a^2 - b^2$')
+        Parte2 = Tex(f"Qua é o valor da expressão:\\\\$a^2 - b^2$")
         Parte2.scale(2)
 
-        Parte3 = Tex(f'Qua é o valor da expressão:\\\\$(a - b)(a + b)$')
+        Parte3 = Tex(f"Qua é o valor da expressão:\\\\$(a - b)(a + b)$")
         Parte3.scale(2)
 
-        Parte4 = Tex(f'Qua é o valor da expressão:\\\\$(2015 - 2014)(2015 + 2014)$')
+        Parte4 = Tex(f"Qua é o valor da expressão:\\\\$(2015 - 2014)(2015 + 2014)$")
         Parte4.scale(2)
 
-        Parte5 = Tex(f'Resultado:\\\\$4029$')
+        Parte5 = Tex(f"Resultado:\\\\$4029$")
         Parte5.scale(2)
 
         # Animação
@@ -45,9 +49,10 @@ class Problema(Scene):
         self.play(FadeOut(Parte5))
 
 
+# Agradecimentos:
 class Agradecimento(Scene):
     def construct(self) -> None:
-        Ins = Tex(f'Inscreva-se para\\\\mais vídeos!')
+        Ins = Tex(f"Inscreva-se para\\\\mais vídeos!")
         Ins.scale(2)
 
         self.play(Write(Ins))
